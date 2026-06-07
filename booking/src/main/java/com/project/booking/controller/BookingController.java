c18690af2ead0dff2565c46a88bd7b9db95ff2cc
Mohit-Singh-007 <msms789549@gmail.com>
2026-06-07 17:17:34 +0530

    idempotency - create-booking

--- booking/src/main/java/com/project/booking/controller/BookingController.java
+++ booking/src/main/java/com/project/booking/controller/BookingController.java
@@ -2,24 +2,48 @@ package com.project.booking.controller;
 
 import com.project.booking.dto.BookingReq;
 import com.project.booking.dto.BookingRes;
+import com.project.booking.idempotency.IdempotencyService;
 import com.project.booking.service.impl.BookingService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/bookings")
 @RequiredArgsConstructor
+@Slf4j
 public class BookingController {
 
     private final BookingService bookingService;
+    private final IdempotencyService idempotencyService;
 
     @PostMapping
-    public ResponseEntity<BookingRes> createBooking(@RequestBody @Valid BookingReq req) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(bookingService.createBooking(req));
+    public ResponseEntity<BookingRes> createBooking(@RequestBody @Valid BookingReq req
+    ,@RequestHeader(value = "Idempotency-Key",required = false) String idempotencyKey) {
+
+        // if no key -> process normally
+        if(idempotencyKey==null || idempotencyKey.isBlank()){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(bookingService.createBooking(req));
+        }
+
+        // check in redis for existing res
+        Optional<BookingRes> existing = idempotencyService.getResult(idempotencyKey,BookingRes.class);
+        if(existing.isPresent()){
+            log.info("Returning cached result for Idempotency-Key {}", idempotencyKey);
+            return ResponseEntity.ok(existing.get());
+        }
+
+        // process and cache it
+        BookingRes res = bookingService.createBooking(req);
+        idempotencyService.saveResult(idempotencyKey,res);
+        return ResponseEntity.status(HttpStatus.CREATED).body(res);
+
     }
 
     @GetMapping("/{id}")
---
+++ booking/src/main/java/com/project/booking/idempotency/IdempotencyService.java
@@ -0,0 +1,61 @@
+package com.project.booking.idempotency;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+/*
+* attach unique idempotency key to each req -> store in redis
+* */
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class IdempotencyService {
+
+    private final RedisTemplate<String,String> redisTemplate;
+    private final ObjectMapper objectMapper;
+
+    private static final String PREFIX = "idempotency:";
+    private static final long TTL_HOURS = 24;
+
+
+
+    public <T> Optional<T> getResult(String key , Class<T> type){
+        String redisKey = PREFIX+key;
+        String cached = redisTemplate.opsForValue().get(redisKey);
+
+        if(cached == null) return Optional.empty();
+
+        try{
+            log.info("Idempotency key hit: {}",key);
+            return Optional.of(objectMapper.readValue(cached,type));
+        }catch (Exception e){
+            log.warn("Failed to deserialize idempotency result for key {}", key);
+            return Optional.empty();
+        }
+    }
+
+    public <T> void saveResult(String key, T result){
+        try {
+            String redisKey = PREFIX+key;
+
+            redisTemplate.opsForValue().set(
+                    redisKey,
+                    objectMapper.writeValueAsString(result),
+                    TTL_HOURS,
+                    TimeUnit.HOURS
+            );
+            log.info("Idempotency result saved for key {}", key);
+        }catch (Exception e){
+            log.warn("Failed to save idempotency result for key {}", e.getMessage());
+        }
+    }
+
+
+}
